4954210c00130f0c7bfac250e3226d0d60768bd2
angginaruto <angginaruto774@gmail.com>
2026-04-21 11:11:40 +0700

    cleanup

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+const config = {
+    preset: "ts-jest/presets/default-esm",
+    testEnvironment: "node",
+    extensionsToTreatAsEsm: [".ts"],
+    moduleNameMapper: {
+        "^(\\.{1,2}/.*)\\.js$": "$1",
+    },
+    transform: {
+        "^.+\\.tsx?$": [
+            "ts-jest",
+            {
+                useESM: true,
+                tsconfig: {
+                    module: "ESNext",
+                    moduleResolution: "bundler",
+                },
+            },
+        ],
+    },
+    testMatch: ["**/__tests__/**/*.test.ts"],
+    clearMocks: true,
+};
+export default config;
